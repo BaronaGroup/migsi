a4c0dc926c6986cee7dd9f9b3156559fcad0be8d
Tapani Haka <tapani.haka@barona.fi>
2017-10-12 17:59:24 +0300

    Allow skipping eslint test with an environment variable
    This can be helpful when developing new tests, so that you do not need to be 100% compliant at all times

--- test/eslint-test.js
+++ test/eslint-test.js
@@ -1,7 +1,7 @@
 const cp = require('child_process')
 
 describe('eslint-test', function() {
-  it('eslint should pass', function() {
+  it('eslint should pass', process.env.MIGSI_SKIP_ESLINT ? undefined : function() {
     cp.execSync('npm run eslint')
   })
 })
