41460b6e776e8d3b6cac28c63a4ce4b73541fb27
Tapani Haka <tapani.haka@barona.fi>
2017-10-16 14:22:48 +0300

    Fix using "native" code on node 8+

--- require-best.js
+++ require-best.js
@@ -1,5 +1,7 @@
+
 module.exports = function(moduleName) {
-  var majorVersion = parseInt(process.version.match(/^v(\d+)/) || [])[1]
+  var majorVersion = parseInt((process.version.match(/^v(\d+)/) || [])[1])
+
   if (majorVersion >= 8) {
     return require('./src/' + moduleName)
   } else {
